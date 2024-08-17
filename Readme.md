dadd7d88165c18030a3a473af207a8637dd84d4d
Matthew Mitchell <mazchoo@gmail.com>
2024-08-17 19:12:16 +0700

    Added loading of wasm

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
 `rustup override set nightly`
+
+Search user settings in the command pallette with Ctrl + Shift + P
+Then add the following entry to the settings
+"rust-analyzer.procMacro.ignored": {
+    "leptos_macro": [
+        // optional:
+        // "component",
+        "server"
+    ],
+}
---
+++ leptos-controller/src/lib.rs
@@ -0,0 +1,42 @@
+use wasm_bindgen::prelude::*;
+use leptos::*;
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    logging::log!("Hello to the console {}", name);
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count(count() + 1);
+                mount_to_body(|| view!{ <p> "That is cool" </p> })
+            }
+            class:red=move || count() % 2 == 1 // Will toggle a class
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count()}
+        </button>
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_buffer_udpate() -> Vec<f32> {
+    return vec![1., 1. , 1., 10.];
+}
+
+#[wasm_bindgen]
+pub fn do_more_stuff() {
+    logging::log!("Hello to the console");
+    console_error_panic_hook::set_once();
+    mount_to_body(|| view!{ 
+        <App/>
+        <p> "Hello there, you noob!"</p>
+    });
+}
