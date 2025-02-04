6cf8e38b9a16559408096946973d7abfe434aaeb
Matthew Mitchell <mazchoo@gmail.com>
2025-02-04 23:07:12 +0700

    Added callbacks to get selected canvas points

--- src/fabricHandle.ts
+++ src/fabricHandle.ts
@@ -1,23 +1,39 @@
 import * as fabric from 'fabric';
 
-const FABRIC_CANVAS_HANDLER = new fabric.Canvas('fabric-canvas');
+export const FABRIC_CANVAS_HANDLER = new fabric.Canvas('fabric-canvas');
 
-export let HIGHLIGHT_RECT: fabric.Rect | undefined = undefined;
+let HIGHLIGHT_RECT: fabric.Rect | null = null;
 
 export function inialiseRect(minX: number, minY: number, maxX: number, maxY: number) {
-    const HIGHLIGHT_RECT = new fabric.Rect({
+    if (HIGHLIGHT_RECT !== null) return;
+
+    HIGHLIGHT_RECT = new fabric.Rect({
         left: minX,
         top: minY,
         fill: 'transparent',
-        stroke: 'red',
+        stroke: 'blue',
         strokeWidth: 1,
         width: maxX - minX,
         height: maxY - minY,
         angle: 0,     
         selectable: true,
     });
+
     FABRIC_CANVAS_HANDLER.add(HIGHLIGHT_RECT);
     FABRIC_CANVAS_HANDLER.setActiveObject(HIGHLIGHT_RECT);
+}
+
+export function updateRect(minX: number, minY: number, maxX: number, maxY: number) {
+    if (HIGHLIGHT_RECT === null) return;
+    HIGHLIGHT_RECT.set({ left: minX, top: minY, width: maxX - minX, height: maxY - minY});
+    HIGHLIGHT_RECT.setCoords();
+    FABRIC_CANVAS_HANDLER.requestRenderAll();
+}
+
+export function clearCanvas() {
+    if (HIGHLIGHT_RECT === null) return;
+    FABRIC_CANVAS_HANDLER.clear();
+    HIGHLIGHT_RECT = null;
 }
 
 export function initialiseCanvas(height: number, width: number) {
@@ -37,3 +53,13 @@ export function enableSelection() {
 export function disableSelection() {
     FABRIC_CANVAS_HANDLER.selection = false;
 }
+
+FABRIC_CANVAS_HANDLER.on('mouse:down', (e) => {
+    if (!FABRIC_CANVAS_HANDLER.selection) return;
+    console.log('Start', e.scenePoint);
+});
+
+FABRIC_CANVAS_HANDLER.on('mouse:up', (e) => {
+    if (!FABRIC_CANVAS_HANDLER.selection) return;
+    console.log('End', e.scenePoint);
+})
