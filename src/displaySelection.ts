a611416114d46b9b6e66a365b5fd4f9033f2e1d7
Matthew Mitchell <mazchoo@gmail.com>
2025-09-27 14:08:06 +0700

    Added display of highlighted offset

--- src/displaySelection.ts
+++ src/displaySelection.ts
@@ -3,8 +3,14 @@
 import { ISettings } from './interfaces/settings';
 
 const NR_OBJECTS_SELECTED = document.getElementById('nr-objects-selected') as HTMLInputElement;
+const OFFSET_SELECTED = document.getElementById('offset-selected') as HTMLInputElement;
+
+function formatted2dPoint(point: [number, number]): string {
+  return `${point[0].toFixed(2)}, ${point[1].toFixed(2)}`;
+}
 
 export function updateBottomBarDisplay(settings: ISettings) {
   console.log(settings);
   NR_OBJECTS_SELECTED.value = String(settings.highlight_nr_selected_entities);
+  OFFSET_SELECTED.value = formatted2dPoint(settings.highlight_offset);
 }
